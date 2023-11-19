cce47973fe4993d4ccfab08ab7be6f05f1a9e9ca
Hojoon Kim <hojoon0724@gmail.com>
2023-11-18 21:56:34 -0800

    Array Methods with Callbacks finished

--- CallbackHW/combining_datatypes.js
+++ CallbackHW/combining_datatypes.js
@@ -124,7 +124,7 @@ calcCallback(82, 7, multiplyTwoNums);
 calcCallback(244, 4, divideTwoNums);
 
 // Function definition placement
-// Clean up this code, so that it works and has function definitions in the correct place
+//? Clean up this code, so that it works and has function definitions in the correct place
 const bar = () => {
   console.log("bar here");
 };
@@ -137,7 +137,7 @@ foo1();
 bar();
 
 // Error reading
-// What is meant by the error(s) this produces?
+//? What is meant by the error(s) this produces?
 
 /*
 foo();
@@ -180,3 +180,73 @@ const panagram = ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy"
 
 // The first question is for the numbers array. The second question is for the words array.
 // You don't have to write an answer to the thought questions.
+
+// Every
+// Determine if every number is greater than or equal to 0
+let everyNumIsZero = nums.every((num) => num >= 0);
+console.log(everyNumIsZero);
+// determine if every word shorter than 8 characters
+let shorterThanEightChars = panagram.every((ltr) => ltr.length < 8);
+console.log(shorterThanEightChars);
+
+// Filter
+// filter the array for numbers less than 4
+let filterLessThanFour = nums.filter((num) => num < 4);
+console.log(filterLessThanFour);
+// filter words that have an even length
+let filterEvenLengthWords = panagram.filter((word) => word.length % 2 === 0);
+console.log(filterEvenLengthWords);
+
+// Find
+// Find the first value divisible by 5
+let firstDivByFive = nums.find((num) => num % 5 === 0);
+console.log(firstDivByFive);
+// find the first word that is longer than 5 characters
+let wordLongerThanFive = panagram.find((word) => word.length > 5);
+console.log(wordLongerThanFive);
+
+// Find Index
+// find the index of the first number that is divisible by 3
+let divisblByThree = nums.findIndex((num) => num % 3 === 0);
+console.log(divisblByThree);
+// find the index of the first word that is less than 2 characters long
+let lessThanTwoChars = panagram.findIndex((word) => word.length < 2);
+console.log(lessThanTwoChars);
+
+// For Each
+// console.log each value of the nums array multiplied by 3
+let multiByThree = nums.forEach((num) => console.log(num * 3));
+// console.log each word with an exclamation point at the end of it
+let addExclToWord = panagram.forEach((word) => console.log(`${word}!`));
+
+// Thought Questions
+// What happened to the original array?
+//*Nothing. The functions are reading the array elements but nothing is being written
+// Can you store the values from a forEach method in a new array?
+let newNumsArray = [];
+let pushNums = nums.forEach((num) => newNumsArray.push(num));
+console.log(newNumsArray);
+
+// Map
+// make a new array of each number multiplied by 100
+let makeNumCentury = nums.map((num) => num * 100);
+console.log(makeNumCentury);
+// make a new array of all the words in all uppercase
+let makeUpperCase = panagram.map((word) => word.toUpperCase());
+console.log(makeUpperCase);
+
+// Thought Questions
+// What happened to the original array?
+//*Nothing. Everything is only being read. New processed things have been sent to a new array
+// Can you store the values from a map method in a new array?
+let newNewArray = [];
+newNewArray = makeNumCentury;
+console.log(newNewArray);
+
+// Some
+// Find out if some numbers are divisible by 7
+let someDivblBySeven = nums.some((num) => num % 7 === 0);
+console.log(`Some are divisible by 7`, someDivblBySeven);
+// Find out if some words have the letter a in them
+let someLtrIsA = panagram.some((word) => word.split("").includes("a") === true);
+console.log(someLtrIsA);
