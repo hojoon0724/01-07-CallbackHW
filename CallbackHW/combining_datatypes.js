4111a43e76a71405eb0c1f9ea30070b801e20eb2
Hojoon Kim <hojoon0724@gmail.com>
2023-11-18 20:38:19 -0800

    callbacks math functions done

--- CallbackHW/combining_datatypes.js
+++ CallbackHW/combining_datatypes.js
@@ -28,7 +28,7 @@ const apartmentBuilding = [
   { unit: 3, tenants: ["Jessica Miller", "Jordan Brown", "Taylor Anderson"] },
 ];
 
-console.log(apartmentBuilding[2].tenants[0]);
+console.log(apartmentBuilding[0].tenants[0]);
 
 // Create a function knit that returns an object that has the following kinds of properties item: scarf, size : 6ft etc. Log a value of that object (hint: call the function and then call a property on the return value).
 
@@ -62,3 +62,63 @@ const powerButton = () => {
 };
 
 powerButton();
+
+// Model a Vending Machine
+// a vending machine is an object
+// it has an array of snacks (make 3 snacks)
+// snacks are objects that have a name and a price
+// a vending machine has a function vend that allows user to enter the array position (a number) of the snack and then that snack will be returned
+// Be able to call vendingMachine.vend() with a valid integer to return a snack
+
+const vendingMachine = {
+  snacks: [
+    { name: "pringles", price: 10 },
+    { name: "doritos", price: 6 },
+    { name: "snickers", price: 3 },
+  ],
+  vend: (i) => {
+    console.log(`here's your ${vendingMachine.snacks[i].name}`);
+  },
+};
+
+vendingMachine.vend(0);
+
+// -----------------------------------------------------
+// Callbacks
+// -----------------------------------------------------
+
+// Make a function add that takes two arguments (numbers) and sums them together
+function addTwoNums(a, b) {
+  let answer = a + b;
+  return answer;
+}
+
+// Make a function subtract that takes two arguments (numbers) and subtracts them
+function subtractTwoNums(a, b) {
+  let answer = a + b;
+  return answer;
+}
+
+// Make a function multiply that takes two arguments and multiplies them
+function multiplyTwoNums(a, b) {
+  let answer = a * b;
+  return answer;
+}
+
+// Make a function divide that takes two arguments and divides them
+function divideTwoNums(a, b) {
+  let answer = a / b;
+  return answer;
+}
+
+// Make a function calculate that takes three arguments. Assume the two arguments are a number ie num1, num2 and a function called operates (a callback).
+// Make it so that when calculate is invoked, the callback "operates" on the numbers and returns the value.
+// Call calculate 4 times, each time using one of the operation functions you wrote
+function calcCallback(num1, num2, cbFunc) {
+  console.log(cbFunc(num1, num2));
+}
+
+calcCallback(4, 8, addTwoNums);
+calcCallback(9, 74, subtractTwoNums);
+calcCallback(82, 7, multiplyTwoNums);
+calcCallback(244, 4, divideTwoNums);
